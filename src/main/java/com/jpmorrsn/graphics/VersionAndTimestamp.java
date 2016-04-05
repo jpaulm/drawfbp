635ec3b3aaaa2e97aa968a771f099ffa11fc765b
Paul Morrison <jpaulmorr@gmail.com>
2016-04-05 14:49:05 -0400

    Updated version no. and timestamp﻿

--- src/main/java/com/jpmorrsn/graphics/VersionAndTimestamp.java
+++ src/main/java/com/jpmorrsn/graphics/VersionAndTimestamp.java
@@ -5,9 +5,9 @@ package com.jpmorrsn.graphics;
 
 public final class VersionAndTimestamp {
 
-	static String version = "2.10.17";
+	static String version = "2.10.18";
 
-	static String date = "Apr. 4, 2016";
+	static String date = "Apr. 5, 2016";
 
 	static String getVersion() {
 		return version;
