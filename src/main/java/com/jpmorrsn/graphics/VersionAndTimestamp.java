2eb79cada4873243dbb87841ed661851ed63b30a
Paul Morrison <jpaulmorr@gmail.com>
2016-06-23 11:44:57 -0400

    Forgot to update timestamp﻿

--- src/main/java/com/jpmorrsn/graphics/VersionAndTimestamp.java
+++ src/main/java/com/jpmorrsn/graphics/VersionAndTimestamp.java
@@ -5,9 +5,9 @@ package com.jpmorrsn.graphics;
 
 public final class VersionAndTimestamp {
 
-	static String version = "2.11.2";
+	static String version = "2.11.3";
 
-	static String date = "June 19, 2016";
+	static String date = "June 23, 2016";
 
 	static String getVersion() {
 		return version;
