b8b69e89ffc3edeaf1a7383e26e69a2f7c9d9e63
Paul Morrison <jpaulmorr@gmail.com>
2021-05-03 14:17:51 -0400

    Update drawfbp.bat

--- drawfbp.bat
+++ drawfbp.bat
@@ -2,5 +2,5 @@ while(true){
 echo Displaying "%1"
 pause
 }
-java -verbose -jar "C:\Users\Paul\Documents\GitHub\drawfbp\build\libs\drawfbp-2.20.10.jar" %1
+java -jar "C:\Users\Paul\Documents\GitHub\drawfbp\build\libs\drawfbp-2.20.10.jar" %1
 pause
