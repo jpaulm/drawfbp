c2bf3a0ff15d4af64193555a3b3a9f0cd5f9e6e1
Paul Morrison <jpaulmorr@gmail.com>
2021-06-08 11:42:54 -0400

    Modified drawfbp.bat﻿

--- drawfbp.bat
+++ drawfbp.bat
@@ -1,6 +1,5 @@
 echo off
 echo DrawFBP displaying "%1"
 echo Jar file: "%DRAWFBP_LIB%\%DRAWFBP_VERSION%.jar"
-cd "%DRAWFBP_LIB%"
-%JAVA_HOME%\bin\java.exe -jar %DRAWFBP_VERSION%.jar %1
 pause
+"%JAVA_HOME%\bin\javaw.exe" -jar "%DRAWFBP_LIB%\build\libs\%DRAWFBP_VERSION%.jar" %1
